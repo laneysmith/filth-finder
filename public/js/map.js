0daa3c364eb505f9839acb48c0a237ba580607ab
Laney Smith <lsmith.unc@gmail.com>
2016-05-19 23:06:50 -0600

    MVP

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,9 @@
 var map;
 var infowindow;
 var service;
-var data = [];
 var address;
 var addressGeo;
 var yelpData = [];
-
-// Pulls location from local storage when called inside initMap()
-function getLocation() {
-	var localData = localStorage.getItem('filthFinderLocation')
-	address = localData;
-}
 
 // Initializes the map.
 function initMap() {
@@ -20,61 +13,23 @@ function initMap() {
     zoom: 14
   });
 	getLocation();
-	geocodeAddress();
-	$.post("https://galvanize-yelp-api.herokuapp.com/search", {
-		"term": "food",
-		"radius_filter": 2000,
-		// "limit": 20,
-		// "offset": 20,
-		"category_filter": 'convenience,hotdogs,donuts',
-		"sort": 1, // Returns results by distance
-		"location": address
-	})
-	.done(function (results) {
-		var path = results.businesses
-		console.log(results.businesses)
-		for (var i = 0; i < path.length; i++) {
-			if (results.businesses[i].is_closed == false) {
-				var dataItem = {};
-				dataItem.name = path[i].name;
-				dataItem.lat = path[i].location.coordinate.latitude;
-				dataItem.lng = path[i].location.coordinate.longitude;
-				dataItem.address = path[i].location.address[0] + ', ' + path[i].location.city + ', ' + path[i].location.state_code
-				dataItem.distance = Math.round(path[i].distance);
-				dataItem.rating = path[i].rating;
-				dataItem.reviewCt = path[i].review_count;
-				dataItem.categories = [].concat.apply([], path[i].categories);
-				dataItem.filthiness = filthRating(dataItem.categories, dataItem.rating);
-				yelpData.push(dataItem);
-			}
-		}
-		yelpData = yelpData.sort(function(a, b) {
-	    return b.filthiness - a.filthiness;
-	  });
-		console.log(yelpData)
-	  yelpData.forEach(function(data) {
-	      var trash = trashCanRating(data.filthiness);
-	      $('tbody').append('<tr><td><i class="fa fa-map-marker"></i> ' + data.name + '<br><small>' + dataItem.address + '</small>' + '</td><td>' + data.distance + '</td><td>' + trash + '</td></tr>');
-	    });
-	});
 }
 
-function filthRating(flatCat, rating) {
-	var points = 0
-	console.log(flatCat)
-	if (flatCat.includes('hotdogs') || flatCat.includes('donuts') || flatCat.includes('convenience')) {
-		points+= 4
+// Pulls location from local storage when called inside initMap()
+function getLocation() {
+	if (localStorage.hasOwnProperty('filthFinderLocation')) {
+		address = localStorage.getItem('filthFinderLocation');
+		geocodeAddress(address);
+		yelpStuff();
+	} else if (localStorage.hasOwnProperty('filthFinderCurrLoc')) {
+		addressGeo = JSON.parse(localStorage.getItem('filthFinderCurrLoc'));
+		console.log(addressGeo);
+		map.setCenter(addressGeo);
+		address = reverseGeocode(addressGeo);
+		yelpStuff();
+	} else {
+		alert('fuck');
 	}
-	if (flatCat.includes('cupcakes') || flatCat.includes('food_court')) {
-		points+= 4
-	}
-	if (flatCat.includes('mexican') || flatCat.includes('burgers') || flatCat.includes('hotdog') || flatCat.includes('candy') || flatCat.includes('cheesesteaks') || flatCat.includes('chicken_wings')) {
-		points++
-	}
-	if (rating < 3) {
-		points+= 1
-	}
-	return points
 }
 
 // geocodeAddress() converts the address/city inputted into the form into
@@ -87,80 +42,85 @@ function geocodeAddress() {
       if (status == google.maps.GeocoderStatus.OK) {
         addressGeo = results[0].geometry.location;
         map.setCenter(addressGeo);
-        updateMap(addressGeo);
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
   });
 }
 
-function updateMap(addressGeo) {
-  infowindow = new google.maps.InfoWindow();
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: addressGeo,
-    openNow: true,
-    zoom: 14,
-    radius: 2500,
-    type: ['food']
-    }, callback);
+function yelpStuff() {
+	$.post("https://galvanize-yelp-api.herokuapp.com/search", {
+		"term": "food",
+		"radius_filter": 2000,
+		"category_filter": 'convenience,hotdogs,donuts',
+		"sort": 1, // Returns results by distance
+		"location": address
+	})
+	.done(function (results) {
+		var path = results.businesses;
+		for (var i = 0; i < path.length; i++) {
+			if (results.businesses[i].is_closed === false) {
+				var dataItem = {};
+				dataItem.name = path[i].name;
+				dataItem.lat = path[i].location.coordinate.latitude;
+				dataItem.lng = path[i].location.coordinate.longitude;
+				dataItem.address = path[i].location.address[0] + ', ' + path[i].location.city + ', ' + path[i].location.state_code;
+				dataItem.distance = Math.round(path[i].distance);
+				dataItem.rating = path[i].rating;
+				dataItem.reviewCt = path[i].review_count;
+				dataItem.categories = [].concat.apply([], path[i].categories);
+				dataItem.filthiness = filthRating(dataItem.categories, dataItem.rating);
+				yelpData.push(dataItem);
+				createMarker(path[i], dataItem.filthiness);
+			}
+		}
+		yelpData = yelpData.sort(function(a, b) {
+			return b.filthiness - a.filthiness;
+		});
+		console.log(yelpData);
+		yelpData.forEach(function(data) {
+			if (isNaN(data.distance)) {
+				var trash = trashCanRating(data.filthiness);
+				$('tbody').append('<tr><td><i class="fa fa-map-marker"></i> ' + data.name + '<br><small>' + dataItem.address + '</small>' + '</td><td>' + '</td><td>' + trash + '</td></tr>');
+			} else {
+				var trash = trashCanRating(data.filthiness);
+				$('tbody').append('<tr><td><i class="fa fa-map-marker"></i> ' + data.name + '<br><small>' + dataItem.address + '</small>' + '</td><td>' + data.distance + '</td><td>' + trash + '</td></tr>');
+			}
+			});
+	});
 }
 
-function callback(results, status, pagination) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    // console.log(results)
-    // $('tbody').empty();
-    // setMapOnAll(null);
-    data = [];
-    for (var i = 0; i < results.length; i++) {
-      var dataItem = {};
-      dataItem.name = results[i].name;
-      dataItem.lat = results[i].geometry.location.lat;
-      dataItem.lng = results[i].geometry.location.lng;
-      dataItem.vicitinty = results[i].vicinity;
-      dataItem.types = results[i].types;
-      dataItem.rating = results[i].rating;
-      dataItem.price_level = results[i].price_level;
-      dataItem.filthiness = numRating(results[i]);
-      data.push(dataItem);
-      var filthiness = numRating(results[i]);
-      createMarker(results[i], filthiness);
-      // if (pagination.hasNextPage) {
-      //   var moreButton = document.getElementById('more');
-      //   moreButton.disabled = false;
-      //   moreButton.addEventListener('click', function() {
-      //     moreButton.disabled = true;
-      //     pagination.nextPage();
-      //   });
-      // }
-    }
-  }
-  // data = data.sort(function(a, b) {
-  //   return b.filthiness - a.filthiness;
-  // });
-  // // console.log(data)
-  // data.forEach(function(data) {
-  //     var trash = trashCanRating(data.filthiness);
-  //     $('tbody').append('<tr><td><i class="fa fa-map-marker"></i> ' + data.name + '</td><td>' + '</td><td>' + trash + '</td></tr>');
-  //   });
+function filthRating(flatCat, rating) {
+	var points = 0;
+	if (flatCat.includes('hotdogs') || flatCat.includes('donuts') || flatCat.includes('convenience')) {
+		points+= 4;
+	}
+	if (flatCat.includes('cupcakes') || flatCat.includes('food_court')) {
+		points+= 4;
+	}
+	if (flatCat.includes('mexican') || flatCat.includes('burgers') || flatCat.includes('hotdog') || flatCat.includes('candy') || flatCat.includes('cheesesteaks') || flatCat.includes('chicken_wings')) {
+		points++;
+	}
+	if (rating < 3) {
+		points+= 1;
+	}
+	return points;
 }
 
-// Calculates the numeric "filth rating" based on price level, overall rating,
-// types.
-function numRating(result) {
-  var ratings = [];
-  if (isNaN(result.price_level) === false) {
-    ratings.push((result.price_level/4)*5);
-  }
-  if (isNaN(result.rating) === false) {
-    ratings.push(result.rating);
-  }
-  var sum = 0;
-  for (var i=0; i < ratings.length; i++) {
-    sum += ratings[i];
-  }
-  var rating = 5-(sum/ratings.length);
-  return rating;
+// Creates markers with labels on the map.
+function createMarker(place, filthiness) {
+  var placeLoc = {};
+	placeLoc.lat = place.location.coordinate.latitude;
+	placeLoc.lng = place.location.coordinate.longitude;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: placeLoc
+  });
+	infowindow = new google.maps.InfoWindow();
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent('<strong>' + place.name + '</strong><br>' + trashCanRating(filthiness));
+    infowindow.open(map, this);
+  });
 }
 
 // Translates numeric rating to trash can equivalent.
@@ -181,15 +141,26 @@ function trashCanRating(rating) {
   return trash;
 }
 
-// Creates markers with labels on the map.
-function createMarker(place, filthiness) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent('<strong>' + place.name + '</strong><br>' + trashCanRating(filthiness));
-    infowindow.open(map, this);
+function reverseGeocode(geocoder, map, infowindow) {
+	var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({'location': addressGeo}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+				address = results[1].formatted_address;
+				console.log(address);
+        // map.setZoom(11);
+        // var marker = new google.maps.Marker({
+        //   position: latlng,
+        //   map: map
+        // });
+        // updateMap(results[1].formatted_address);
+				// infowindow.setContent(results[1].formatted_address);
+        // infowindow.open(map, marker);
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
   });
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,34 @@
 $(document).ready(function() {
 var address;
 
-  // Saves address to local storage so it can be retrieved on the results page.
-  function saveLocation(address) {
-    var str = address;
-    localStorage.setItem('filthFinderLocation', str);
-  }
-
   // If user manually inputs address, geocodeAddress is called to Translate it
   // into coordinate pairs.
-  $( "form" ).submit(function( event ) {
+  $('form').submit(function(event) {
     event.preventDefault();
+    localStorage.clear();
     address = $('#address').val();
-    saveLocation(address)
-    window.location = "results.html"
+    localStorage.setItem('filthFinderLocation', address);
+    // saveLocation(address)
+    window.location = 'results.html'
   });
 
   // If user
-  $( "#currentLocation" ).click(function() {
+  $('#currentLocation').click(function() {
+    localStorage.clear();
     // findCurrentLocation() uses the browser to capture the coordinates of the
     // user's current location. It is called when the user clicks the "Use
     // current location" button. If the coordinates are successfully captured,
     // the map is updated to center around the new coordinate pair.
-      // if (!navigator.geolocation){
-      //   alert(status);
-      //   return;
-      // }
-      navigator.geolocation.getCurrentPosition(success, error);
-      function success(position) {
-        // addressGeo = {};
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var addressGeo = {};
         addressGeo.lat = position.coords.latitude;
         addressGeo.lng = position.coords.longitude;
-        // map.setCenter(addressGeo);
-        // updateMap(addressGeo);
-      }
-      function error() {
-        alert(status);
-      }
+        localStorage.setItem('filthFinderCurrLoc', JSON.stringify(addressGeo))
+        window.location = 'results.html'
+      })
+    }
+
   });
 
 });
